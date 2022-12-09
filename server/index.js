144807a829c4dacf03880258c43b3b91739c3993
shubh9752 <61161071+shubh9752@users.noreply.github.com>
2022-12-09 16:38:17 +0530

    routes created

--- server/index.js
+++ server/index.js
@@ -1,1 +1,27 @@
 const express =require("express");
+const cors=require("cors");
+const mongoose = require("mongoose");
+
+const app=express();
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+}).then(()=>{
+    console.log("DB connection established")
+}).catch((err)=>{
+    console.log(err.message);
+});
+
+const server =app.listen(process.env.PORT,(err)=>{
+    if(err){
+        console.log(`there is an error in running server ${err}`);
+    }
+    console.log(`Your server is running at port ${process.env.PORT}`);
+
+});
